724edcc95fd130d9ca1f2771a28b6253d7b24c08
NOGiTTiS <Knight_Darkwing@hotmail.com>
2024-12-25 08:04:03 +0700

    v4@favicon

--- views/auth/login.php
+++ views/auth/login.php
@@ -11,6 +11,7 @@ session_start();
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="../assets/css/style.css">
 <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link rel="icon" href="/assets/img/logo.png" type="image/png">
 <style>
 body {
 background-color: #f8e8ee;
